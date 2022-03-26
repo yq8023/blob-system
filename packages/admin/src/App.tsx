a531870769b2aac812ef6b61ad0d982c3c36825c
杨清 <yangqing.0106@bytedance.com>
2022-03-26 15:08:04 +0800

    feat: admin web 项目新建

---
+++ packages/admin/src/App.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import { Button } from "antd";
+function App() {
+  return (
+    <div>
+      <header>管理后台</header>
+      <Button type="primary">Button</Button>
+    </div>
+  );
+}
+
+export default App;
---
+++ packages/web/src/App.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import { Button } from "antd";
+function App() {
+  return (
+    <div>
+      <header>前台</header>
+      <Button type="primary">Button</Button>
+    </div>
+  );
+}
+
+export default App;
